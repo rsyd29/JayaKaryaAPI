3a9612151d59afa5dacc91564e2b884b83303d0e
rsyd29 <brasyid15@gmail.com>
2021-06-29 18:08:12 +0700

    first commit

---
+++ JayaKaryaAPI/Models/JayaKaryaAPIContext.cs
@@ -0,0 +1,22 @@
+﻿using System.Data.Entity;
+
+namespace JayaKaryaAPI.Models
+{
+    public class JayaKaryaAPIContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, add the following
+        // code to the Application_Start method in your Global.asax file.
+        // Note: this will destroy and re-create your database with every model change.
+        // 
+        // System.Data.Entity.Database.SetInitializer(new System.Data.Entity.DropCreateDatabaseIfModelChanges<JayaKaryaAPI.Models.JayaKaryaAPIContext>());
+
+        public JayaKaryaAPIContext() : base("name=JayaKaryaAPIContext")
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ JayaKaryaAPI/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JayaKaryaAPI.Models
+{
+    public class Product
+    {
+        // Id, Name, Price, Stock
+        public int Id { set; get; }
+        public String Name { set; get; }
+        public double Price { set; get; }
+        public int Stock { set; get; }
+    }
+}
